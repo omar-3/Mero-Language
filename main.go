0eb419e69f239b9b31c6f68b0c37873584957210
omar-3 <s-omarkhaled@zewailcity.edu.eg>
2021-06-15 19:07:22 +0200

    yo

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"compiler/repl"
+	"fmt"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the MERO programming language!\n",
+		user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
