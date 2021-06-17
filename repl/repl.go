9ea470cf8946a32fac9e163bb3448d124a365da0
omar-3 <s-omarkhaled@zewailcity.edu.eg>
2021-06-17 17:35:40 +0200

    hash'

--- repl/repl.go
+++ repl/repl.go
@@ -55,8 +55,6 @@ const MONKEY_FACE = `            __,__
 `
 
 func printParserErrors(out io.Writer, errors []string) {
-	io.WriteString(out, MONKEY_FACE)
-	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
 	io.WriteString(out, " parser errors:\n")
 	for _, msg := range errors {
 		io.WriteString(out, "\t"+msg+"\n")
